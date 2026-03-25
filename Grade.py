485b71bbe40d91407b3de84d5d9ef85cb1bbb28f
saran9208 <saranpandi9208@gmail.com>
2026-03-25 21:05:52 +0530

    Grade.py

---
+++ Grade.py
@@ -0,0 +1,11 @@
+mark = int(input())
+if mark >= 90:
+    print("A Grade")
+elif mark >= 75:
+    print("B Grade")
+elif mark >= 60:
+    print("C Grade")
+elif mark >= 50:
+    print("D Grade")
+else:
+    print("F Grade")
